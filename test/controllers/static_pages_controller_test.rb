751f65de750595b6fe2856abfee1bf274371721e
henktillman <henk.tillman@gmail.com>
2015-03-18 03:08:01 +0000

    Initial commit to layout branch

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "CS 232 Ruby on Rails Development"
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
