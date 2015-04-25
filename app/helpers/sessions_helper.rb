d663f26e2307ed517b9c42bcd85adf9575990615
Henk Tillman <henk.tillman@gmail.com>
2015-04-24 18:57:44 -0700

    Remember me checkbox and tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,12 +8,12 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns the current logged-in user (if any)
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if(user_id = session[:user_id])
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
@@ -29,8 +29,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
